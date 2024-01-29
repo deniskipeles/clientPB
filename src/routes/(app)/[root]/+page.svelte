8da0d11a86e0d2244dad348d54e83c18603aea57
Kipeles Kemboi <47624821+deniskipeles@users.noreply.github.com>
2024-01-29 09:30:02 +0300

    Update +page.svelte

--- src/routes/(app)/[root]/+page.svelte
+++ src/routes/(app)/[root]/+page.svelte
@@ -4,18 +4,16 @@
 	import { P } from 'flowbite-svelte';
 
 	$: home_page = $page.data?.roots?.find((i) => i?.name?.includes('home page')) ?? {};
-	$: about_page = $page.data?.roots?.find((i) => i?.name?.includes('about page')) ?? {};
+	$: root_page = $page.data?.roots?.find((i) => i?.id?.includes($page?.url?.params?.root)) ?? {};
 	$: school = $page.data?.roots?.find((i) => i?.name?.includes('school')) ?? {};
 </script>
 
 <svelte:head>
-	<title>{school?.data?.name ?? 'Prime School'}</title>
-	<meta name="description" content={school?.data?.decription ?? 'Prime School Online Resources'} />
+	<title>{root_page?.data?.name ?? 'Prime School'}</title>
+	<meta name="description" content={root_page?.data?.decription ?? 'Prime School Online Resources'} />
 </svelte:head>
 <div class="pt-20">
-	<!-- <P class="px-8 py-4">
-		{@html home_page?.html_data}
-	</P> -->
+	
 	<TinyMce
 		cssClass="tinymce-preview custom-scrollbar"
 		conf={{
@@ -32,7 +30,7 @@
 			toolbar: '',
 			plugins: ['autoresize']
 		}}
-		value={about_page?.html_data ?? home_page?.html_data}
+		value={root_page?.html_data ?? home_page?.html_data}
 		disabled
 	/>
 </div>
