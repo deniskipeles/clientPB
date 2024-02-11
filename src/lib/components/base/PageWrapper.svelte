9974e7f141776e99e3555ed0c2874685fac07ba9
Kipeles Kemboi <47624821+deniskipeles@users.noreply.github.com>
2024-02-11 10:30:42 +0300

    Update PageWrapper.svelte

--- src/lib/components/base/PageWrapper.svelte
+++ src/lib/components/base/PageWrapper.svelte
@@ -13,13 +13,13 @@
     <footer class="page-footer">
         <slot name="footer" />
 
-        <a href={import.meta.env.PB_DOCS_URL} target="_blank" rel="noopener noreferrer">
+        <a href={import.meta.env.PB_DOCS_URL ?? "https://www.ktechs.xyz"} target="_blank" rel="noopener noreferrer">
             <i class="ri-book-open-line txt-sm" />
-            <span class="txt">Docs</span>
+            <span class="txt">Owner</span>
         </a>
         <span class="delimiter">|</span>
-        <a href={import.meta.env.PB_RELEASES} target="_blank" rel="noopener noreferrer" title="Releases">
-            <span class="txt">kit-pb-school {import.meta.env.PB_VERSION ?? 1.0}</span>
+        <a href={import.meta.env.PB_RELEASES ?? "https://www.ktechs.xyz"} target="_blank" rel="noopener noreferrer" title="Releases">
+            <span class="txt">k-schools {import.meta.env.PB_VERSION ?? "1.0"}</span>
         </a>
     </footer>
 </div>
