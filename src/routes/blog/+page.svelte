dca054abdedf3969343c08646d0f181e2e604b9b
Kipeles Kemboi <47624821+deniskipeles@users.noreply.github.com>
2024-03-18 14:35:44 +0300

    Update +page.svelte

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -8,19 +8,15 @@
   import { invalidateAll } from '$app/navigation';
   import { page } from '$app/stores';
   import RecordUpsertPanel from '$lib/components/records/RecordUpsertPanel.svelte';
-	
 
   /** @type {import('./$types').PageData} */
   export let data;
-	
-	let collection = $page?.data?.tables?.find((t)=> t?.name=="blog");
-	let collectionUpsert = $page?.data?.tables?.find((t)=> t?.name=="blog");
-	let recordUpsertPanel;
-	
-	
-    $: categories = [];
-    const categoryField = collection?.schema?.find(field => field?.name === 'category');
-    categories = categoryField?.options?.values ?? [];
+
+  let collection = $page?.data?.tables?.find((t)=> t?.name=="blog");
+  let collectionUpsert = $page?.data?.tables?.find((t)=> t?.name=="blog");
+  let recordUpsertPanel;
+
+  $: categories = collection?.schema?.find(field => field?.name === 'category')?.options?.values || [];
 </script>
 
 <Breadcrumb class="pt-20 py-8">
@@ -28,47 +24,51 @@
   <BreadcrumbItem>Articles</BreadcrumbItem>
 </Breadcrumb>
 
-
 <div>
   {#if $page?.data?.user && $page?.data?.user.roles?.includes('blogger')}
-					<button type="button" class="btn btn-expanded" on:click={() => recordUpsertPanel?.show()}>
-						<i class="ri-add-line" />
-						<span class="txt">New record</span>
-					</button>
-				{/if}
-	
-<ul>
-  {#each categories as category (category)}
-    <li><button class="bg-blue-500 text-white px-4 py-2 rounded">{category}</button></li>
-  {/each}
-</ul>
+    <button type="button" class="btn btn-expanded" on:click={() => recordUpsertPanel?.show()}>
+      <i class="ri-add-line" />
+      <span class="txt">New record</span>
+    </button>
+  {/if}
+
+  <ul class="flex space-x-2">
+    {#each categories as category (category)}
+      <li>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          {category}
+        </button>
+      </li>
+    {/each}
+  </ul>
+
   <div class="grid relative md:grid-cols-3 gap-6">
     {#each data?.articles?.items ?? [] as article (article?.id)}
-    <Card>
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{article?.title}</h5>
-      <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-        {article?.content}
-      </p>
-      <Button href={`/blog/${article?.id}`} class="w-fit">
-        Read more
-      </Button>
-    </Card>
+      <Card>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {article?.title}
+        </h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+          {article?.content}
+        </p>
+        <Button href={`/blog/${article?.id}`} class="w-fit">
+          Read more
+        </Button>
+      </Card>
     {/each}
   </div>
 </div>
 
 <RecordUpsertPanel
-	bind:this={recordUpsertPanel}
-	collection={collectionUpsert}
-	on:hide={() => {
-	 recordUpsertPanel?.hide()
-	}}
-	on:save={(e) => {
-		invalidateAll();
-	}}
-	on:delete={(e) => {
-		invalidateAll();
-	}}
+  bind:this={recordUpsertPanel}
+  collection={collectionUpsert}
+  on:hide={() => {
+    recordUpsertPanel?.hide()
+  }}
+  on:save={(e) => {
+    invalidateAll();
+  }}
+  on:delete={(e) => {
+    invalidateAll();
+  }}
 />
-
-
