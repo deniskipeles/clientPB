faf92a79508aa4898c818245a730c0476b339651
deniskipeles <narydbest@gmail.com>
2024-05-22 11:15:39 +0300

    added ai prompt input

--- src/lib/components/RecordsActionsButtons.svelte
+++ src/lib/components/RecordsActionsButtons.svelte
@@ -207,7 +207,7 @@
         }) ?? [];
     }) ?? [];
     
-    let tableData=tableDataFxn()
+    $: tableData=tableDataFxn()
     $: if(fullyLoaded&&schema?.length){
       tableData=tableDataFxn()
     }
@@ -218,7 +218,7 @@
     <div class="flex gap-3 text-center dark:text-white">
         <AskAI context={{
           headerData:schema?.map((i) => i?.name?.replaceAll('_', ' ')) ?? [],
-          bodyData:tableData
+          bodyData:tableDataFxn()
         }}/>
         <button
             use:tooltip={`Click here to download pdf of the content you are viewing currently.(${filds} fields)`}
