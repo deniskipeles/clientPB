86662b821c4d483285e8ff9c26e1c53f487a9f5f
deniskipeles <narydbest@gmail.com>
2024-05-29 16:05:52 +0300

    added iframe for preview

---
+++ src/lib/components/base/Iframe.svelte
@@ -0,0 +1,144 @@
+<script>
+  import { createEventDispatcher, onMount } from 'svelte';
+  import CommonHelper from "$lib/utils/CommonHelper";
+  import { processHTMLString } from "$lib/utils/latexProcessor";
+  import tooltip from '$lib/actions/tooltip';
+
+  export let markdown;
+  export let id = `iframe-${CommonHelper.randomString(7)}`;
+  export let scriptSources = [
+    'https://i.upmath.me/latex.js',
+    'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js',
+  ];
+
+  let marked;
+  const dispatch = createEventDispatcher();
+
+  $: if (markdown) {
+    markdown = processHTMLString(markdown);
+    if (marked) markdown = marked.parse(markdown);
+    if (iframe?.contentDocument?.body?.innerHTML) {
+      iframe.contentDocument.body.innerHTML = marked ? marked.parse(markdown) : markdown;
+    }
+  }
+
+  onMount(() => {
+    loadMarked();
+    try {
+      iframeFxn();
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  const loadMarked = () => {
+    if (!window.marked) {
+      const script = document.createElement('script');
+      script.src = 'https://cdn.jsdelivr.net/npm/marked@12.0.2/lib/marked.umd.min.js';
+      document.head.append(script);
+
+      script.onload = () => {
+        marked = window.marked.marked;
+        console.log('marked loaded');
+      };
+    } else {
+      marked = window.marked.marked;
+    }
+  };
+
+  const uniqueDivId = `print${CommonHelper.randomString(7)}`;
+  let iframe;
+  let loadingFrame = true;
+
+  const iframeFxn = () => {
+    const doc = iframe.contentDocument;
+    const head = doc.head;
+    const body = doc.body;
+
+    // Inject styles
+    const style = doc.createElement('style');
+    style.textContent = `
+      /* General Styles */
+      body {
+        font-family: Arial, sans-serif;
+        font-size: 10pt;
+        line-height: 1.5;
+        color: #000;
+      }
+      /* Headings */
+      h1, h2, h3, h4, h5, h6 {
+        page-break-after: avoid;
+      }
+      /* Prevent page break within elements */
+      p, blockquote, pre, table, figure {
+        page-break-inside: avoid;
+      }
+      /* Tables */
+      table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-bottom: 1em;
+      }
+      th, td {
+        border: 1px solid #000;
+        padding: 0.5em;
+        text-align: left;
+      }
+      th {
+        background-color: #f2f2f2;
+      }
+      /* Images */
+      img {
+        max-width: 100%;
+        height: auto;
+      }
+      /* Links */
+      a {
+        color: #418df3;
+        text-decoration: none;
+      }
+      a:visited {
+        color: #3e44e8;
+      }
+      /* Lists */
+      ul, ol {
+        margin: 0 0 1em 1em;
+      }
+      /* Code blocks */
+      pre, code {
+        font-family: "Courier New", Courier, monospace;
+        font-size: 10pt;
+      }
+      /* Avoid breaking code blocks */
+      pre {
+        page-break-inside: avoid;
+      }
+    `;
+    head.appendChild(style);
+
+    // Inject content
+    body.innerHTML = marked ? marked.parse(markdown) : markdown;
+
+    // Inject scripts
+    scriptSources.forEach((src) => {
+      const script = doc.createElement('script');
+      script.src = src;
+      head.appendChild(script);
+    });
+    loadingFrame = false;
+  };
+</script>
+
+<style>
+  iframe {
+    width: 100%;
+    height: 1000px;
+  }
+</style>
+
+<iframe
+  title="Article"
+  id={`microapp-${uniqueDivId}`}
+  frameborder="0"
+  bind:this={iframe}
+></iframe>
--- src/lib/components/base/PreviewHtml.svelte
+++ src/lib/components/base/PreviewHtml.svelte
@@ -1,187 +1,53 @@
 <script>
-	import { createEventDispatcher, onMount } from 'svelte';
-	import CommonHelper from "$lib/utils/CommonHelper";
-	import LatexImage from "$lib/components/base/LatexImage.svelte";
-	import {processHTMLString} from "$lib/utils/latexProcessor";
-	import tooltip from '$lib/actions/tooltip';
-	
-	
-	
-	
-	export let markdown;
-	export let id = "tinymce_svelte" + CommonHelper.randomString(7);
-	
-	let marked;
-	const dispatch = createEventDispatcher();
+  import CommonHelper from "$lib/utils/CommonHelper";
+  import { processHTMLString } from "$lib/utils/latexProcessor";
+  import tooltip from '$lib/actions/tooltip';
+  import { Printer, CloudArrowDown } from 'svelte-heros-v2';
+  import { printFxn, genPDF } from '$lib/utils';
+  import Iframe from '$lib/components/base/Iframe.svelte';
 
-	$: if (markdown) {
-	  markdown = processHTMLString(markdown)
-		if(marked)markdown = marked.parse(markdown);
-	}
-	
-	onMount(()=>{
-		loadMarked()
-		loadMathjax()
-		try{iframeFxn()}catch(e){console.log(e)}
-	})
-	const loadMarked = () => {
-	  if(!window.marked){
-  		let script = document.createElement('script');
-      script.src = "https://cdn.jsdelivr.net/npm/marked@12.0.2/lib/marked.umd.min.js";
-      document.head.append(script);
-  		
-  		script.onload = () => {
-        marked = window.marked.marked
-  			console.log("marked loaded")
-  		};
-	  }else{
-        marked = window.marked.marked
-	  }
-	}
-	
-	const loadMathjax = () => {
-		let script = document.createElement('script');
-    script.src = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js";
-    document.head.append(script);
-		
-		script.onload = () => {
-      MathJax = {
-        tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]},
-      };
-		};
-		console.log("mathjax loaded")
-	}
-	
-	import { Printer, CloudArrowDown } from 'svelte-heros-v2';
-  import { printFxn,genPDF } from '$lib/utils';
-  const uniqueDivId = "print" + CommonHelper.randomString(7);
-  
-  $: generating = false
-  let printingPDF = false;
-  
-  const genPDF_fxn = async()=> await genPDF('https://aiwebapp-rwci.onrender.com/',markdown,(options)=>{
-    generating=options.generating;
-    // file=options.file;
-  })
-  
-  
-  export let content = "";
-  export let scriptSources = [
-    "https://i.upmath.me/latex.js",
-    "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"
-    ]; // Array of script URLs to inject
-  let iframe;
-  let loadingFrame=true;
-  const iframeFxn=() => {
-    const doc = iframe.contentDocument;
-    const head = doc.head;
-    const body = doc.body;
-
-    // Inject styles
-    const style = doc.createElement('style');
-    style.textContent = `
-    /* General Styles */
-body {
-    font-family: Arial, sans-serif;
-    font-size: 10pt;
-    line-height: 1.5;
-    color: #000;
-}
-/* Headings */
-h1, h2, h3, h4, h5, h6 {
-    page-break-after: avoid;
-}
-/* Prevent page break within elements */
-p, blockquote, pre, table, figure {
-    page-break-inside: avoid;
-}
-/* Tables */
-table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-bottom: 1em;
-}
-th, td {
-    border: 1px solid #000;
-    padding: 0.5em;
-    text-align: left;
-}
-th {
-    background-color: #f2f2f2;
-}
-/* Images */
-img {
-    max-width: 100%;
-    height: auto;
-}
-/* Links */
-a {
-    color: #000;
-    text-decoration: none;
-}
-a:visited {
-    color: #000;
-}
-/* Lists */
-ul, ol {
-    margin: 0 0 1em 1em;
-}
-/* Code blocks */
-pre, code {
-    font-family: "Courier New", Courier, monospace;
-    font-size: 10pt;
-}
-/* Avoid breaking code blocks */
-pre {
-    page-break-inside: avoid;
-}
-    `
-    head.appendChild(style);
-    // Inject content
-    body.innerHTML = marked?marked.parse(markdown):markdown;
-
-    // Inject scripts
-    scriptSources.forEach(src => {
-      const script = doc.createElement('script');
-      script.src = src;
-      
-      head.appendChild(script);
-    });
-    loadingFrame=false
+  export let markdown;
+  $: if (markdown) {
+    markdown = processHTMLString(markdown);
   }
-  
+
+  const uniqueDivId = `print-${CommonHelper.randomString(7)}`;
+
+  let generating = false;
+  let printingPDF = false;
+
+  const genPDF_fxn = async () => {
+    await genPDF('https://aiwebapp-rwci.onrender.com/', markdown, (options) => {
+      generating = options.generating;
+      // file = options.file;
+    });
+  };
 </script>
 
-
-<style>
-    iframe { width: 100%; height:1000px}
-</style>
-
-
-<iframe
-  title="Article"
-  id='microapp-{uniqueDivId}'
-  frameborder='0'
-  bind:this={iframe}
-></iframe>
+<div id={uniqueDivId}>
+  <Iframe {markdown} />
+</div>
 
 <div class="flex">
-	<button type="button" class="btn btn-transparent" on:click={genPDF_fxn}>
-		<span class="txt {generating ? 'animate-ping' : ''}">
-		  <CloudArrowDown /> {'pdf'}
-		</span>
-	</button>|
   <button
-      use:tooltip={`Click here to download or print what you see in this table in pdf format.`}
-      class={`btn btn-transparent ${printingPDF ? 'animate-ping' : ''}`}
-      on:click={() => {
-          printFxn(uniqueDivId,uniqueDivId,iframe?.contentDocument?.body?.innerHTML??markdown);
-      }}
+    type="button"
+    class="btn btn-transparent"
+    on:click={genPDF_fxn}
+  >
+    <span class={`txt ${generating ? 'animate-ping' : ''}`}>
+      <CloudArrowDown /> {'pdf'}
+    </span>
+  </button>
+  |
+  <button
+    use:tooltip={`Click here to download or print what you see in this table in pdf format.`}
+    class={`btn btn-transparent ${printingPDF ? 'animate-ping' : ''}`}
+    on:click={() => {
+      printFxn(uniqueDivId, uniqueDivId, markdown);
+    }}
   >
     <span class="txt">
-      <Printer />{'print'}
+      <Printer /> {'print'}
     </span>
   </button>
 </div>
-
-
